cc4d007f7e0992bb393449c945bd7b840f050af6
Toby <torbjorn.nilsen@gmail.com>
2022-08-13 23:53:20 +1000

    CSS utility.

---
+++ src/justright/html/css.js
@@ -0,0 +1,111 @@
+import { ArrayUtils, List, StringUtils } from "coreutil_v1";
+import { BaseElement } from "../element/baseElement";
+
+export class CSS {
+    
+    /**
+     * @type {BaseElement}
+     */
+    static from(baseElement) {
+        return new CSS(baseElement);
+    }
+
+    /**
+     * 
+     * @param {BaseElement} baseElement 
+     */
+    constructor(baseElement) {
+        /** @type {BaseElement} */
+        this.baseElement = baseElement;
+    }
+
+    clear() {
+        this.baseElement.setAttributeValue("class", "");
+        return this;
+    }
+
+    /**
+     * 
+     * @param {String} cssClass 
+     */
+    toggle(cssClass) {
+        let currentClass = this.baseElement.getAttributeValue("class");
+        let currentClassArray = StringUtils.toArray(currentClass, " ");
+        let currentClassList = new List(currentClassArray);
+        
+        if (currentClassList.contains(cssClass)) {
+            currentClassList.remove(cssClass);
+        } else {
+            currentClassList.add(cssClass);
+        }
+        
+        this.baseElement.setAttributeValue("class", ArrayUtils.toString(currentClassList.getArray(), " "));
+        return this;
+    }
+
+    /**
+     * 
+     * @param {String} cssClass 
+     */
+    enable(cssClass) {
+        let currentClass = this.baseElement.getAttributeValue("class");
+        let currentClassArray = StringUtils.toArray(currentClass, " ");
+        let currentClassList = new List(currentClassArray);
+        
+        if (!currentClassList.contains(cssClass)) {
+            currentClassList.add(cssClass);
+        }
+        
+        this.baseElement.setAttributeValue("class", ArrayUtils.toString(currentClassList.getArray(), " "));
+        return this;
+    }
+
+    /**
+     * 
+     * @param {String} cssClass 
+     */
+    disable(cssClass) {
+        let currentClass = this.baseElement.getAttributeValue("class");
+        let currentClassArray = StringUtils.toArray(currentClass, " ");
+        let currentClassList = new List(currentClassArray);
+        
+        if (currentClassList.contains(cssClass)) {
+            currentClassList.remove(cssClass);
+        }
+
+        this.baseElement.setAttributeValue("class", ArrayUtils.toString(currentClassList.getArray(), " "));
+        return this;
+    }
+
+    /**
+     * 
+     * @param {String} cssClassRemovalPrefix 
+     * @param {String} cssClass
+     */
+    replace(cssClassRemovalPrefix, cssClass) {
+        let currentClass = this.baseElement.getAttributeValue("class");
+        let currentClassArray = StringUtils.toArray(currentClass, " ");
+        let currentClassList = new List(currentClassArray);
+        let toRemoveArray = [];
+
+        if (!StringUtils.isBlank(cssClassRemovalPrefix)) {
+            currentClassList.forEach((value) => {
+                if (value.startsWith(cssClassRemovalPrefix)) {
+                    toRemoveArray.push(value);
+                }
+                return true;
+            }, this);
+        }
+
+        toRemoveArray.forEach((toRemoveValue) => {
+            currentClassList.remove(toRemoveValue)
+        });
+
+        currentClassList.add(cssClass);
+        this.baseElement.setAttributeValue("class", ArrayUtils.toString(currentClassList.getArray(), " "));
+        return this;
+    }
+
+    
+
+}
