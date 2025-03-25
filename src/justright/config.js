47af952f98342357c1782086c301606b033610e1
torbjorn.nilsen@gmail.com <torbjorn.nilsen@gmail.com>
2025-03-25 15:38:00 +1100

    StateManager DI config.

--- src/justright/config.js
+++ src/justright/config.js
@@ -1,9 +1,10 @@
 import { Logger, List } from "coreutil_v1"
-import { SingletonConfig } from "mindi_v1"
+import { PrototypeConfig, SingletonConfig } from "mindi_v1"
 import { TemplateRegistry } from "./template/templateRegistry.js";
 import { UniqueIdRegistry } from "./component/uniqueIdRegistry.js";
 import { ComponentFactory } from "./component/componentFactory.js";
 import { StylesRegistry } from "./styles/stylesRegistry.js";
+import { StateManager } from "./state/stateManager.js";
 
 const LOG = new Logger("Config");
 
@@ -18,7 +19,8 @@ export class Config {
             SingletonConfig.unnamed(TemplateRegistry),
             SingletonConfig.unnamed(StylesRegistry),
             SingletonConfig.unnamed(UniqueIdRegistry),
-            SingletonConfig.unnamed(ComponentFactory)]);
+            SingletonConfig.unnamed(ComponentFactory),
+            PrototypeConfig.unnamed(StateManager)]);
         }
 
     getTypeConfigList() {
