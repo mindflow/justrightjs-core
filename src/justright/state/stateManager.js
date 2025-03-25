d091b7f2a919d6764c7dbcde738b2a6b3ce9789f
torbjorn.nilsen@gmail.com <torbjorn.nilsen@gmail.com>
2025-03-25 11:56:26 +1100

    Introduced state manager.

---
+++ src/justright/state/stateManager.js
@@ -0,0 +1,85 @@
+import { Method } from "coreutil_v1";
+
+/**
+ * StateManager
+ * 
+ * @template T
+ */
+export class StateManager {
+
+    constructor() {
+        /** @type {Map<String, any>} */
+        this.objectMap = new Map();
+
+        /** @type {Map<String, Array<Method>} */
+        this.listeners = new Map();
+    }
+
+    /**
+     * 
+     * @param {Method} listener 
+     */
+    react(listener) {
+        const anyKey = "__ANY__";
+        if (!this.listeners.has(anyKey)) {
+            this.listeners.set(anyKey, new Array());
+        }
+        this.listeners.get(anyKey).push(listener);
+    }
+
+    /**
+     * 
+     * @param {string} key 
+     * @param {Method} listener 
+     */
+    reactTo(key, listener) {
+        if (!this.listeners.has(key)) {
+            this.listeners.set(key, new Array());
+        }
+        this.listeners.get(key).push(listener);
+    }
+
+    /**
+     * Update the state
+     * 
+     * @param {string} key 
+     * @param {T} object 
+     */
+    async update(object, key = "__DEFAULT__") {
+        if (Array.isArray(object)) {
+            for (let i = 0; i < object.length; i++) {
+                object[i] = this.createProxy(object[i], key, this);
+            }
+        }
+        object = this.createProxy(object, key, this);
+        this.objectMap.set(key, object);
+        this.signalStateChange(object, key);
+        return object;
+    }
+
+    signalStateChange(object, key) {
+        if (this.listeners.has(key)) {
+            for (let listener of this.listeners.get(key)) {
+                listener.call([object]);
+            }
+        }
+
+        const anyKey = "__ANY__";
+        if (key != anyKey && this.listeners.has(anyKey)) {
+            for (let listener of this.listeners.get(anyKey)) {
+                listener.call([object]);
+            }
+        }
+    }
+
+    createProxy(object, key, stateManager) {
+        return new Proxy(object, {
+            set: (target, prop, value) => {
+                const success = (target[prop] = value);
+                stateManager.signalStateChange(target, key);
+                return success === value;
+            }
+        });
+    }
+
+}
